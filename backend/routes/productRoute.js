0be96168bfdceb356a1cad6cadbc4367346ea117
Ikramul-m <i20727172@gmail.com>
2024-05-31 11:02:00 +0530

    Create Product

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/productCOntroller");
+const { getAllProducts , createProduct } = require("../controllers/productCOntroller");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts)
+router.route("/products/new").get(createProduct)
 
 module.exports = router;
