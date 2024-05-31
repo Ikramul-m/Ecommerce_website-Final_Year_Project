dc0ed5c34025f51803194b856a7ddfd0316eb637
Ikramul-m <i20727172@gmail.com>
2024-05-31 10:19:02 +0530

    Product Model - Product Schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,34 +1,47 @@
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
+
+
   name: {
     type: String,
     required: [true, "Please enter name"],
     trim: true,
   },
+
+
   description: {
     type: String,
     required: [true, "Please enter product description"],
   },
+
+
   price: {
     type: Number,
     required: [true, "Please enter price"],
     maxLength: [8, "Price cannot exceed 8 characters"],
   },
+
+
   rating: {
     type: Number,
     default: 0,
   },
+
+
   images: [
     {
       public_id: {
         type: String,
         required: true,
       },
+      
       url: {
         type: String,
         required: true,
       },
     },
   ],
+
+
 });
