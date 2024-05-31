c3bca7a23dc6aa11fde8340698fc095dea8b4930
Ikramul-m <i20727172@gmail.com>
2024-05-31 13:48:18 +0530

    Update Product -- Adim

--- backend/controllers/productCOntroller.js
+++ backend/controllers/productCOntroller.js
@@ -1,9 +1,6 @@
 const Product = require("../models/productModel");
 
 
-
-
-
 // Create Product -- Admin
 exports.createProduct = async (req, res, next) => {
   try {
@@ -50,8 +47,43 @@ exports.getAllProducts = async (req,res) => {
 
 
 
+// Update product -- Admin
+
+exports.updateProduct = async (req,res) => {
+
+  let product = Product.findById(req.params.id)
+
+  if(!product) {
+    return res.status(500).json({
+      success: false,
+      message: 'Product not found'
+    })
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+    new: true,
+    runValidators: true,
+    useFindAndModify: true
+  })
+
+  res.status(200).json({
+    success: true,
+    product
+  })
+}
 
 
-// exports.getAllProducts = (req, res) => {
-//   res.status(200).json({ message: "Route is working fine" });
-// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
