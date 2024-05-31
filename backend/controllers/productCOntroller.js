7c018ccf06e4eb621467134fe9fe5ffa751e4a00
Ikramul-m <i20727172@gmail.com>
2024-05-31 14:35:14 +0530

    Get Product Details(GET element by id)

--- backend/controllers/productCOntroller.js
+++ backend/controllers/productCOntroller.js
@@ -46,6 +46,28 @@ exports.getAllProducts = async (req,res) => {
 }
 
 
+// Get Product details 
+
+exports.getProductDetails = async (req,res,next) => {
+
+  const product = await Product.findById(req.params.id)
+
+  if(!product) {
+    return res.status(500).json({
+      success: false,
+      message: 'Product not found'
+    })
+  }
+
+  res.status(200).json({
+    success: true,
+    product
+  })
+}
+
+
+
+
 
 // Update product -- Admin
 
@@ -102,6 +124,7 @@ exports.deleteProduct = async (req, res, next) => {
     });
   }
 };
+
 
 
 
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { getAllProducts , createProduct, updateProduct, deleteProduct } = require("../controllers/productCOntroller");
+const { getAllProducts , createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productCOntroller");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts)
 router.route("/product/new").post(createProduct)
-router.route("/product/:id").put(updateProduct).delete(deleteProduct)
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails)
 
 module.exports = router
