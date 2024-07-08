9456140c4eeb10dfdcabea6e4c2e775588d16406
Ikramul-m <i20727172@gmail.com>
2024-07-08 11:59:52 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,31 @@
 const app = require("./app");
+
 const dotenv = require("dotenv");
-const connectDatabase = require('./config/database')
+const connectDatabase = require("./config/database");
+
+// Handling Uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught exceptions`);
+  process.exit(1);
+});
 
 dotenv.config({ path: "backend/config/config.env" });
 
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server listening on http://localhost:${process.env.PORT}`);
 });
 
+// console.log(yttdtiiu);
 
+// Unhandeled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandeled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
