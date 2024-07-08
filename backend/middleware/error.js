fec7fd60c5b2dfd1a7da97ef085eccc84feccdc2
Ikramul-m <i20727172@gmail.com>
2024-07-08 12:57:08 +0530

    Backend Error Handling complete!

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,13 +1,17 @@
-const ErrorHandler = require('../utils/errorhander')
+const ErrorHandler = require("../utils/errorhander");
 
-module.exports = (err,req,res,next) => {
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal server error";
 
-    err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal server error";
+  // Working MongoDB Id error
+  if (err.name === "CastError") {
+    const messsage = `Resource not found, Invalid: ${err.path}`;
+    err = new ErrorHandler(messsage, 400);
+  }
 
-    res.status(err.statusCode).json({
-        success: false,
-        message: err.message
-    })
-}
-
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
